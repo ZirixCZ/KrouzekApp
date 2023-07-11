96f34512687afd2f04d68f1782bc2720f428b4d8
Michal Vaniš <zirixcz@gmail.com>
2023-07-11 14:22:50 +0200

    navigate to /topics & TODO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,29 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import Input from "../components/Input";
 import AuthButton from "../components/AuthButton";
 
 const Auth = () => {
   const [code, setCode] = useState("");
+  const router = useRouter();
 
   const handleSetCode = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCode(e.target.value.toUpperCase());
   };
 
-  useEffect(() => {
-    console.log(code);
-  }, [code]);
+  const handleButtonClick = () => {
+    // TODO: check if code is valid against firebase
+    if (code) {
+      router.push("/topics");
+    }
+  };
+
   return (
     <div className="flex w-full h-full justify-center items-center">
       <div className="flex items-end gap-x-4">
-        <Input handleChange={handleSetCode} />
-        {code && <AuthButton />}
+        <Input handleChange={handleSetCode} value={code} />
+        {code && <AuthButton handleClick={handleButtonClick} />}
       </div>
     </div>
   );
--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 
-const AuthButton = () => {
+interface AuthButtonProps {
+  handleClick: () => void;
+}
+
+const AuthButton = ({ handleClick }: AuthButtonProps) => {
   return (
-    <button className="bg-authbutton-background flex items-center justify-center rounded-md h-11 w-16">
+    <button
+      onClick={handleClick}
+      className="bg-authbutton-background flex items-center justify-center rounded-md h-11 w-16"
+    >
       <Image src="/img/arrow.svg" width={20} height={20} alt="arrow" />
     </button>
   );
