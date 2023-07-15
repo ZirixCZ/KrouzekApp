924657d1ae740e339c471a50d6dbae06d7970330
Michal Vaniš <zirixcz@gmail.com>
2023-07-15 16:42:40 +0200

    put btn into input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,16 +16,16 @@ const Auth = () => {
   const handleButtonClick = () => {
     // TODO: check if code is valid against firebase
     if (code) {
-      router.push("/topics");
+      router.push(`${code}/topics`);
     }
   };
 
   return (
     <div className="flex w-full h-full justify-center items-center">
-      <div className="flex items-end gap-x-4">
+      <div className="flex items-end justify-end gap-x-4 ">
         <Input handleChange={handleSetCode} value={code} />
         {code && (
-          <div className="h-11 w-16">
+          <div className="h-11 w-16 absolute">
             <Button handleClick={handleButtonClick}>
               <Image src="/img/arrow.svg" width={20} height={20} alt="arrow" />
             </Button>
